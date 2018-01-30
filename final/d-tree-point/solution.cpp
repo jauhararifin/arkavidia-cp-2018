93fbb96d2c39d13484860fa7eebc1585026a2eef
Wiwit Rifa'i <wiwitrifai@gmail.com>
2018-01-30 16:28:55 +0700

    remove multiple tc & change input as 0-based

--- final/d-tree-point/solution.cpp
+++ final/d-tree-point/solution.cpp
@@ -17,34 +17,29 @@ void getSize(int v) {
 }
 
 int main() {
-  int t;
-  scanf("%d", &t);
-  while (t--) {
-    int n, k;
-    scanf("%d %d", &n, &k);
-    for (int i = 0; i < n; ++i)
-      child[i].clear();
-    for (int i = 1; i < n; ++i) {
-      int p;
-      scanf("%d", &p);
-      --p;
-      child[p].push_back(i);
-    }
-    getSize(0);
-    dp[0][0] = 1;
-    for (int i = 0; i < n; ++i) {
-      dp[i][0] = 1;
-      for (int j = 1; j <= k; ++j) {
-        dp[i][j] = 0;
-        if (i)
-          dp[i][j] = dp[i-1][j];
-        if (j-sz[i] >= 0) {
-          dp[i][j] += dp[i][j-sz[i]];
-          if (dp[i][j] >= mod) dp[i][j] -= mod;
-        }
+  int n, k;
+  scanf("%d %d", &n, &k);
+  for (int i = 0; i < n; ++i)
+    child[i].clear();
+  for (int i = 1; i < n; ++i) {
+    int p;
+    scanf("%d", &p);
+    child[p].push_back(i);
+  }
+  getSize(0);
+  dp[0][0] = 1;
+  for (int i = 0; i < n; ++i) {
+    dp[i][0] = 1;
+    for (int j = 1; j <= k; ++j) {
+      dp[i][j] = 0;
+      if (i)
+        dp[i][j] = dp[i-1][j];
+      if (j-sz[i] >= 0) {
+        dp[i][j] += dp[i][j-sz[i]];
+        if (dp[i][j] >= mod) dp[i][j] -= mod;
       }
     }
-    printf("%d\n", dp[n-1][k]);
   }
+  printf("%d\n", dp[n-1][k]);
   return 0;
 }
