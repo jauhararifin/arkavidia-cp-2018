139fd412936b3334fd00b9d7fb3142fec0609fa4
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-05 05:43:04 +0700

    add case when computer spec are same and give the worst query

--- final/e-range-time-query/spec.cpp
+++ final/e-range-time-query/spec.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 const int MAXN = 100000;
 const int MAXQ = 100000;
-const int MAXS = 1000000000;
-const int MAXD = 1000000000;
-const int MAXT = 1000000000;
+const int MAXS = 1000;
+const int MAXD = 1000;
+const int MAXT = 1000;
 
 class ProblemSpec : public BaseProblemSpec {
 protected:
@@ -67,6 +67,9 @@ private:
 };
 
 class TestSpec : public BaseTestSpec<ProblemSpec> {
+public:
+  TestSpec():rnd(0xfedbafc) {
+  }
 protected:
   void SampleTestCase1() {
     Input({
@@ -96,31 +99,87 @@ protected:
   }
 
   void TestCases() {
+    // some have same start and same speed, with same LR query
+    CASE(N = MAXN; sameStart(N/2, 1); sameStart(N/2, rnd() % MAXS + 1); sameSpeed(N/2, rnd() % MAXD + 1); randomRestSpeed(); randomRestStart(); Q = MAXQ; sameLRQuery());
+    
+    // some have same start and same speed, with random query
+    CASE(N = MAXN; sameStart(N/2, 1); sameStart(N/2, rnd() % MAXS + 1); sameSpeed(N/2, rnd() % MAXD + 1); randomRestSpeed(); randomRestStart(); Q = MAXQ; randomRestQuery());
+
+    // random computer, with some same LR query
+    CASE(N = MAXN; randomRestComputer(); Q = MAXQ; ; sameLRQuery(); randomRestQuery());
+
+    // random computer with random query
     CASE(N = MAXN; randomRestComputer(); Q = MAXQ; randomRestQuery());
     CASE(N = MAXN; randomRestComputer(); Q = MAXQ; randomRestQuery());
     CASE(N = MAXN; randomRestComputer(); Q = MAXQ; randomRestQuery());
     CASE(N = MAXN; randomRestComputer(); Q = MAXQ; randomRestQuery());
     CASE(N = MAXN; randomRestComputer(); Q = MAXQ; randomRestQuery());
+
+    // worst query
+    CASE(N = MAXN; randomRestComputer(); Q = MAXQ; worstRestQuery());
   }
 
-  void randomRestComputer() {
+private:
+  void sameStart(int n, int start) {
+    for (int i = 0; i < n; i++)
+      A.push_back(start);
+  }
+
+  void sameSpeed(int n, int speed) {
+    for (int i = 0; i < n; i++)
+      B.push_back(speed);
+  }
+
+  void randomRestStart(int maxs = MAXS) {
+    while (A.size() < N) A.push_back(rnd() % maxs + 1);
+  }
+
+  void randomRestSpeed(int maxd = MAXD) {
+    while (B.size() < N) B.push_back(rnd() % maxd + 1);
+  }
+
+  void randomRestComputer(int maxs = MAXS, int maxd = MAXD) {
     while (A.size() < N) {
-      A.push_back(rand() % MAXS + 1);
-      B.push_back(rand() % MAXD + 1);
+      A.push_back(rnd() % MAXS + 1);
+      B.push_back(rnd() % MAXD + 1);
+    }
+  }
+
+  void sameLRQuery(int n = MAXN) {
+    vector<int> r;
+    for (int i = 1; i <= n; i++)
+      r.push_back(i);
+    random_shuffle(r.begin(), r.end());
+
+    for (int i = 0; i < n; i++) {
+      L.push_back(r[i]);
+      R.push_back(r[i]);
+      T.push_back(rnd() % MAXT + 1);
     }
   }
 
   void randomRestQuery() {
     while (L.size() < Q) {
-      int a = rand() % N + 1;
-      int b = rand() % N + 1;
+      int a = rnd() % N + 1;
+      int b = rnd() % N + 1;
       if (a > b)
           swap(a,b);
-      int t = rand() % MAXT + 1;
+      int t = rnd() % MAXT + 1;
       L.push_back(a);
       R.push_back(b);
       T.push_back(t);
     }
   }
+
+  void worstRestQuery() {
+    for (int range = N; range >= 1 && L.size() < Q; range--)
+      for (int i = 1; i <= N - range + 1  && L.size() < Q; i++) {
+        L.push_back(i);
+        R.push_back(i + range - 1);
+        T.push_back(MAXT);
+      }
+  }
+
+  mt19937 rnd;
 
 };
